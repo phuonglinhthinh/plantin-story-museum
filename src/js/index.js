e60b02abe268f82f9a64e843ad5ecdaf69a64407
phuonglinhthinh <phuong.linh.thinh@student.howest.be>
2025-01-22 10:11:55 +0100

    Add lottie player

--- src/js/index.js
+++ src/js/index.js
@@ -1,57 +1,64 @@
-const init = () => {
+import { DotLottie } from '@lottiefiles/dotlottie-web';
 
-  const $navButton = document.querySelector('.nav__button');
-  const $navList = document.querySelector('.nav__list');
-  const $iconLink = document.querySelector('#iconlink');
-  const listItems = $navList.querySelectorAll("li a");
+const $navButton = document.querySelector('.nav__button');
+const $navList = document.querySelector('.nav__list');
+const $iconLink = document.querySelector('#iconlink');
+const listItems = $navList.querySelectorAll("li a");
 
-  $navButton.classList.remove('hidden');
+$navButton.classList.remove('hidden');
+$navList.classList.add("hidden");
+
+const player = new DotLottie({
+  autoplay: true,
+  loop: true,
+  canvas: document.querySelector("#anim"),
+  src: "assets/motion/main.json",
+  marker: "box"
+});
+
+const openNavigation = () => {
+  $navButton.setAttribute("aria-expanded", "true");
+  $iconLink.setAttribute("xlink:href", "#close");
+  $navList.classList.remove("hidden");
+  console.log("working");
+}
+
+const closeNavigation = () => {
+  $navButton.setAttribute("aria-expanded", "false");
+  $iconLink.setAttribute("xlink:href", "#navicon");
   $navList.classList.add("hidden");
+}
 
-  const openNavigation = () => {
-    $navButton.setAttribute("aria-expanded", "true");
-    $iconLink.setAttribute("xlink:href", "#close");
-    $navList.classList.remove("hidden");
-    console.log("working");
-  }
-
-  const closeNavigation = () => {
-    $navButton.setAttribute("aria-expanded", "false");
-    $iconLink.setAttribute("xlink:href", "#navicon");
-    $navList.classList.add("hidden");
-  }
-
-  const toggleNavigation = () => {
-    const open = $navButton.getAttribute("aria-expanded");
-    open === "false" ? openNavigation() : closeNavigation();
-  }
+const toggleNavigation = () => {
+  const open = $navButton.getAttribute("aria-expanded");
+  open === "false" ? openNavigation() : closeNavigation();
+}
 
 
-  const handleBlur = () => {
-    // if (!e.relatedTarget || !$navList.contains(e.relatedTarget)) {
+const handleBlur = () => {
+  // if (!e.relatedTarget || !$navList.contains(e.relatedTarget)) {
+  closeNavigation();
+}
+// }
+
+$navButton.addEventListener("click", toggleNavigation);
+
+// add event to the last item in the nav list to trigger the disclosure to close if the user tabs out of the disclosure
+listItems[listItems.length - 1].addEventListener("blur", handleBlur);
+
+// Close the disclosure if a user presses the escape key
+window.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    $navButton.focus();
     closeNavigation();
   }
-  // }
+});
 
-  $navButton.addEventListener("click", toggleNavigation);
+window.addEventListener('mousemove', handleGlobalUserInteraction);
+window.addEventListener('touchmove', handleGlobalUserInteraction);
+window.addEventListener('scroll', handleGlobalUserInteraction);
+requestAnimationFrame(interactionChecker);
 
-  // add event to the last item in the nav list to trigger the disclosure to close if the user tabs out of the disclosure
-  listItems[listItems.length - 1].addEventListener("blur", handleBlur);
-
-  // Close the disclosure if a user presses the escape key
-  window.addEventListener("keyup", (e) => {
-    if (e.key === "Escape") {
-      $navButton.focus();
-      closeNavigation();
-    }
-  });
-
-  window.addEventListener('mousemove', handleGlobalUserInteraction);
-  window.addEventListener('touchmove', handleGlobalUserInteraction);
-  window.addEventListener('scroll', handleGlobalUserInteraction);
-  requestAnimationFrame(interactionChecker);
-
-}
 
 let lastInteractionTime = 0;
 
@@ -68,4 +75,12 @@ const interactionChecker = () => {
 
 
 
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger, TextPlugin);
+  ScrollTrigger.defaults({ markers: false });
+  openNavigation();
+  closeNavigation();
+  toggleNavigation();
+  $navButton.addEventListener("click", toggleNavigation);
+}
 init();
