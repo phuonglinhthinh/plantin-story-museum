a2f9b212f3e041a728d7b9e7e65c53a2855cd6e8
phuonglinhthinh <phuong.linh.thinh@student.howest.be>
2025-01-25 21:28:38 +0100

    Responsiveness

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,7 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { TextPlugin } from 'gsap/TextPlugin';
-
-
-import { DotLottie } from '@lottiefiles/dotlottie-web';
+import { DotLottie, DotLottieWorker } from '@lottiefiles/dotlottie-web';
 
 const $navButton = document.querySelector('.nav__button');
 const $navList = document.querySelector('.nav__list');
@@ -12,51 +10,51 @@ const $iconLink = document.querySelector('#iconlink');
 $navButton.classList.remove('hidden');
 $navList.classList.add("hidden");
 
-// CANVAS RESIZE
-const resizeCanvas = () => {
-  const canvases = document.querySelectorAll('.canvas');
-  canvases.forEach((canvas) => {
-    canvas.width = 0.7 * window.innerWidth;
-    canvas.height = window.innerHeight - 0.5 * window.innerHeight;
+let heroPlayer = null;
+const createHeroPlayer = () => {
+  if (heroPlayer) {
+    heroPlayer.destroy();
+    heroPlayer = null;
+  }
+
+  const existingCanvas = document.querySelector('#hero__canvas');
+  if (existingCanvas) {
+    existingCanvas.classList.remove();
+    existingCanvas.classList.add('canvas')
+  }
+
+  const isDesktop = window.matchMedia('(min-width: 62em)').matches;
+  const jsonName = isDesktop
+    ? "assets/motion/main.json"
+    : "assets/motion/main.json";
+
+  const canvas = document.querySelector('#hero__canvas')
+  isDesktop ? canvas.classList.add('desktop-motion') : canvas.classList.add('mobile-motion');
+
+  heroPlayer = new DotLottie({
+    autoplay: true,
+    loop: true,
+    canvas: canvas,
+    src: jsonName,
   });
+  heroPlayer.play();
 };
-resizeCanvas();
 
 // Function to control motion based on mouse activity
 let motionTimeout;
-
 const handleMouseMove = () => {
   clearTimeout(motionTimeout);
 
-  // Start the motions if stopped
-  if (player1.state !== "playing") player1.play();
-  if (player2.state !== "playing") player2.play();
+  if (!heroPlayer.isPlaying) heroPlayer.play();
 
-  // Set a timeout to pause the motions after 2 seconds of no movement
   motionTimeout = setTimeout(() => {
-    player1.pause();
-    player2.pause();
+    heroPlayer.pause();
   }, 1000);
 };
 
 // Event listeners for mouse and touch movement
 window.addEventListener("mousemove", handleMouseMove);
 window.addEventListener("touchmove", handleMouseMove);
-
-//MOTION
-const player1 = new DotLottie({
-  autoplay: true,
-  loop: true,
-  canvas: document.querySelector("#anim"),
-  src: "assets/motion/main.json",
-});
-
-const player2 = new DotLottie({
-  autoplay: true,
-  loop: true,
-  canvas: document.querySelector("#anim2"),
-  src: "assets/motion/main0.json",
-});
 
 //PRINTING PRESS
 const steps = [
@@ -125,6 +123,19 @@ const toggleNavigation = () => {
   open === "false" ? openNavigation() : closeNavigation();
 }
 
+// CANVAS RESIZE
+const resizeCanvas = () => {
+  /*const canvases = document.querySelectorAll('.canvas');
+  canvases.forEach((canvas) => {
+    canvas.width = Math.floor(0.7 * window.innerWidth);
+    canvas.height = Math.floor(window.innerHeight - 0.5 * window.innerHeight);
+  });*/
+  plantin.resize();
+  player.resize();
+  createHeroPlayer();
+};
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
 
 const init = () => {
   gsap.registerPlugin(ScrollTrigger, TextPlugin);
@@ -132,7 +143,6 @@ const init = () => {
 
   $navButton.addEventListener("click", toggleNavigation);
 
-  // window.addEventListener('resize', resizeCanvas);
 }
 
 init();
